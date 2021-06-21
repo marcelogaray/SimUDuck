a524b159877f355b264299e9701a155b23c0a25e
marcelogaray <marcelogaray@users.noreply.github.com>
2021-06-21 01:11:38 -0400

    But something went horribly wrong when Rubber duckies were added,  error they doesn't fly

---
+++ src/main/java/org/simuduckapp/review/RubberDuck.java
@@ -0,0 +1,13 @@
+package org.simuduckapp.review;
+
+public class RubberDuck extends Duck{
+
+    @Override
+    public String quack(){
+        return "Squeak";
+    }
+    @Override
+    String display() {
+        return "looks like a rubberduck";
+    }
+}
